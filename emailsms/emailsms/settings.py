83edab94870230bf33c66f6d7bf78d584a9c8752
aniceto-jolela <anicetojosejolelaj@gmail.com>
2024-12-14 21:07:45 +0100

    theme.

--- emailsms/emailsms/settings.py
+++ emailsms/emailsms/settings.py
@@ -128,6 +128,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static', # Replace 'static' with your actual static directory name
+]
 STATIC_URL = "static/"
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = ('bulma',)
